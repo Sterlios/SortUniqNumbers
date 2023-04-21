6967de2ed4fd8d18ed9b2890b337392ea5b92c4d
Sterlios <kuzmin.s.anton@gmail.com>
2023-04-21 22:08:47 +0200

    Change NumberManager.GetData

--- SortUniqNumbers/NumberManager.cs
+++ SortUniqNumbers/NumberManager.cs
@@ -8,20 +8,13 @@ namespace SortUniqNumbers
     {
         private static Random _random = new Random();
 
-        private int _minNumber;
-        private int _maxNumber;
         private List<int> _numbers = new List<int>(1000);
         private List<int> _uniqNumbers = new List<int>(1000);
 
-        public string GetData(int minCount, int maxCount)
-        {
-            if (_minNumber == 0 && _maxNumber == 0)
-                Question.GetRange("\nЧисла в диапазоне:", out _minNumber, out _maxNumber);
+		public string GetData(int minCount, int maxCount) => 
+            _random.Next(minCount, maxCount).ToString();
 
-            return _random.Next(_minNumber, _maxNumber).ToString();
-        }
-
-        public bool TryAdd(string input)
+		public bool TryAdd(string input)
         {
             if (!int.TryParse(input, out int number))
                 return false;
