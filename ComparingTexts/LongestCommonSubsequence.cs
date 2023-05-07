5f86d18e56d58a3b92c2d98704f5a022ddfe36f3
Sterlios <kuzmin.s.anton@gmail.com>
2023-05-07 20:01:46 +0200

    add FillMatrix

--- ComparingTexts/LongestCommonSubsequence.cs
+++ ComparingTexts/LongestCommonSubsequence.cs
@@ -7,16 +7,23 @@ namespace ComparingTexts
 	{
 		public void Compare(string text1, string text2, out IReadOnlyColoredText iColoredText1, out IReadOnlyColoredText iColoredText2)
 		{
+			int[,] matrix = FillMatrix(text1, text2);
+
+			GetResult(matrix, text1, text2, out ColoredText coloredText1, out ColoredText coloredText2);
+
+			iColoredText1 = coloredText1;
+			iColoredText2 = coloredText2;
+		}
+
+		private int[,] FillMatrix(string text1, string text2)
+		{
 			int[,] matrix = new int[text1.Length + 1, text2.Length + 1];
 
 			for (int i = 0; i < text1.Length; i++)
 				for (int j = 0; j < text2.Length; j++)
 					matrix[i + 1, j + 1] = GetLengthSubsequence(text1[i], text2[j], matrix[i, j], matrix[i + 1, j], matrix[i, j + 1]);
 
-			GetResult(matrix, text1, text2, out ColoredText coloredText1, out ColoredText coloredText2);
-
-			iColoredText1 = coloredText1;
-			iColoredText2 = coloredText2;
+			return matrix;
 		}
 
 		private int GetLengthSubsequence(char letter1, char letter2, int diagonalElement, int horizontalElement, int verticalElement)
@@ -30,26 +37,26 @@ namespace ComparingTexts
 		private void GetResult(int[,] matrix, string text1, string text2, out ColoredText coloredText1, out ColoredText coloredText2)
 		{
 			int startPositionInText = 0;
-			int i = matrix.GetLength(0) - 1;
-			int j = matrix.GetLength(1) - 1;
+			int row = matrix.GetLength(0) - 1;
+			int column = matrix.GetLength(1) - 1;
 
 			coloredText1 = new ColoredText();
 			coloredText2 = new ColoredText();
 
-			while (matrix[i, j] > 0)
+			while (matrix[row, column] > 0)
 			{
-				bool isAddedRange1 = TryAddRange(matrix[i, j], matrix[i - 1, j], matrix[i, j - 1], text1, coloredText1, i);
-				bool isAddedRange2 = TryAddRange(matrix[i, j], matrix[i, j - 1], matrix[i - 1, j], text2, coloredText2, j);
+				bool isAddedRange1 = TryAddRange(matrix[row, column], matrix[row - 1, column], matrix[row, column - 1], text1, coloredText1, row);
+				bool isAddedRange2 = TryAddRange(matrix[row, column], matrix[row, column - 1], matrix[row - 1, column], text2, coloredText2, column);
 
 				if (isAddedRange1)
-					i--;
+					row--;
 
 				if (isAddedRange2)
-					j--;
+					column--;
 			}
 
-			AddRange(coloredText1, text1.Substring(startPositionInText, i), ColoredRange.AdditionalRangeColor, startPositionInText);
-			AddRange(coloredText2, text2.Substring(startPositionInText, j), ColoredRange.AdditionalRangeColor, startPositionInText);
+			AddRange(coloredText1, text1.Substring(startPositionInText, row), ColoredRange.AdditionalRangeColor, startPositionInText);
+			AddRange(coloredText2, text2.Substring(startPositionInText, column), ColoredRange.AdditionalRangeColor, startPositionInText);
 		}
 
 		private bool TryAddRange(int currentElement, int previousElementInCurrentText, int previousElementInOtherText, string text, ColoredText coloredText, int index)
